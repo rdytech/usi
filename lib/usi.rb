f44182a0849edb3b50684757944b6085c8b6636d
Sam Giffney <samuelgiffney@gmail.com>
2014-07-31 12:48:51 +1000

    make gem rails 2.3 friendly

--- lib/usi.rb
+++ lib/usi.rb
@@ -4,4 +4,4 @@ end
 require 'usi/version'
 require 'usi/luhn_check'
 require 'usi/validator'
-require 'usi_validator' if defined?(Rails)
+require 'usi_validator' if defined?(Rails) && defined?(ActiveModel)
